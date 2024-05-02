bf8baf346a3e7ec55887b791aaac99f160362cab
Keltars <keltars2000@gmail.com>
2024-05-02 09:40:03 +0300

    2-snackbar complete

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,31 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const delay = Number(form.delay.value);
+  const state = form.state.value;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else if (state === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  })
+    .then(delay =>
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      })
+    )
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
